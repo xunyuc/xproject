29d8025139bbe1f57c140681d1c70781e36c89af
Xunyuc <xxx@xx.com>
2017-06-18 15:16:04 +0800

    init

--- src/main/java/com/xunyuc/xproject/web/utils/XmlUtilEnum.java
+++ src/main/java/com/xunyuc/xproject/web/utils/XmlUtilEnum.java
@@ -4,6 +4,8 @@ import com.xunyuc.xproject.web.bean.ResultBean;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Date;
@@ -19,7 +21,8 @@ public enum XmlUtilEnum {
 
     XmlUtilEnum(){
         jaxb2Marshaller = new Jaxb2Marshaller();
-        jaxb2Marshaller.setPackagesToScan("com.xunyuc.xproject.web");
+        // 指定包里查找有相关jaxb注解的类
+        jaxb2Marshaller.setPackagesToScan("com.xunyuc.xproject");
     }
 
     public String bean2Xml(Object bean){
@@ -28,11 +31,21 @@ public enum XmlUtilEnum {
         return writer.toString();
     }
 
+    public Object xml2Bean(String xml){
+        Object bean = jaxb2Marshaller.unmarshal(new StreamSource(new StringReader(xml)));
+        return bean;
+    }
+
     public static void main(String[] args) {
         ResultBean resultBean = new ResultBean();
         resultBean.setResultCode("002");
         resultBean.setResultMessage("xml");
         resultBean.setResultDate(new Date());
-        System.out.println(XmlUtilEnum.INSTANCE.bean2Xml(resultBean));
+
+        String xml = XmlUtilEnum.INSTANCE.bean2Xml(resultBean);
+        System.out.println(xml);
+        System.out.println(XmlUtilEnum.INSTANCE.xml2Bean(xml));
+
+
     }
 }
